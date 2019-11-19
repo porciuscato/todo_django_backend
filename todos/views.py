ae5b2bbe7ae7970f557a102dca0dcfdc9613bc15
cato <mpcato@naver.com>
2019-11-19 19:24:31 +0900

    add

--- todos/views.py
+++ todos/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .serializers import TodoSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 # Create your views here.
+@api_view(['POST'])
+def todo_create(request):
+    serializer = TodoSerializer(data=request.POST)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(status=400)
